a0e8ca491e3ec18f48e2529551a8473af0fe00ef
shazna <shaznaisthikar@gmail.com>
2026-04-17 23:22:49 +0530

    Added script to Convert Image to Grayscale

---
+++ code/q4.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+IMG_PATH = os.path.join(BASE_DIR, "images", "girl.png")
+OUT_DIR = os.path.join(BASE_DIR, "outputs", "q4")
+
+os.makedirs(OUT_DIR, exist_ok=True)
+
+print(IMG_PATH)
+print(os.path.exists(IMG_PATH))
+
+# Load image
+img = cv2.imread(IMG_PATH)
+print("Image object:", img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imwrite(os.path.join(OUT_DIR, "gray.png"), gray)
